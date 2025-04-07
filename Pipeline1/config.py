1a5c845666176bde61324422b6d7b2e4c4fce2a0
the-prat <chatterjeepratysh2006@gmail.com>
2025-04-07 20:08:07 +0530

    pipeline1 gpt-ed

--- Pipeline1/config.py
+++ Pipeline1/config.py
@@ -2,13 +2,13 @@
 
 import os
 from langchain_groq import ChatGroq
-# from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 # from openai import OpenAI
 from dotenv import load_dotenv
 load_dotenv()
 # Default model configuration
-DEFAULT_MODEL = "gemini-2.0-flash-001"  # You can choose a suitable Groq model
+DEFAULT_MODEL = "gpt-4o-mini"  # You can choose a suitable Groq model
 DEFAULT_TEMPERATURE = 0.2
 
 # Initialize LLM
@@ -18,9 +18,9 @@ def get_llm(model_name=None, temperature=None):
     temp = temperature if temperature is not None else float(os.getenv("LLM_TEMPERATURE", DEFAULT_TEMPERATURE))
     
     # client = OpenAI(api_key=os.getenv("GEMINI_API_KEY"))
-    model='gemini-2.0-flash-001'
-    return ChatGoogleGenerativeAI(
-        api_key=os.getenv("GEMINI_API_KEY"),
+    model='gpt-4o-mini'
+    return ChatOpenAI(
+        api_key=os.getenv("OPEN_AI_API_KEY"),
         model=model,
         temperature=temp,
         max_retries=2
